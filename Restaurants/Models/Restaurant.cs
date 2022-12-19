fc2866eb1f0b291bb3bd3bfd25b3c847a2a807a0
Rafael Petrachini <raffp@me.com>
2022-12-18 16:05:20 -0800

    add necessary packages and initial file structure

---
+++ Restaurants/Models/Restaurant.cs
@@ -0,0 +1,10 @@
+namespace Restaurants.Models
+{
+  public class Restaurant
+  {
+    public int RestaurantId { get; set; }
+    public string Name { get; set; }
+    public int Rating { get; set; }
+    public string Address { get; set; }
+  }
+}
